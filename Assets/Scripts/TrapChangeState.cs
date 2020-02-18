c36bc7cb9128db66f7716c97447f4e88f7c3c7ff
wang3ng <56563071+wang3ng@users.noreply.github.com>
2020-02-18 15:44:58 -0800

    k,

---
+++ Assets/Scripts/TrapChangeState.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrapChangeState : MonoBehaviour
+{
+    public SpriteRenderer spR;
+    // Start is called before the first frame update
+    void Start()
+    {
+        spR = GetComponent<SpriteRenderer>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (spR.sprite.name == "Trap0")
+        {
+            gameObject.tag = "Untagged";
+
+        }
+        else if (spR.sprite.name == "Trap1" || spR.sprite.name == "Trap2")
+        {
+            gameObject.tag = "Trap";
+
+        }
+
+    }
+}
