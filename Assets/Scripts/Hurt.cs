9e655402fa7bd9c833d66c99cfa6255af02ba5db
Tonywww2 <1204484415@qq.com>
2020-02-19 15:47:37 -0800

    cvbn

--- Assets/Scripts/Hurt.cs
+++ Assets/Scripts/Hurt.cs
@@ -15,7 +15,7 @@ public class Hurt : MonoBehaviour
 
     }
 
-    private void OnTriggerEnter2D(Collider2D collision)
+    private void OnTriggerStay2D(Collider2D collision)
     {
         if (collision.gameObject.tag == "Trap")
         {
